5bc19f36c0293ed727b5066457afb64b62b9728f
anikshajol <anikshajol@gmail.com>
2023-09-25 14:50:56 +0600

    banner background image added with overlay

--- src/components/Banner/Banner.jsx
+++ src/components/Banner/Banner.jsx
@@ -1,7 +1,26 @@
+import bannerImage from "../../assets/banner.png";
+
 const Banner = () => {
+  const styles = {
+    banner: {
+      backgroundImage: `url(${bannerImage})`,
+      height: "100vh",
+      backgroundPosition: "center",
+      backgroundSize: "cover",
+      backgroundRepeat: "no-repeat",
+    },
+    content: {
+      height: "100%",
+      width: "100%",
+      backgroundColor: "rgba(255, 255, 255, 0.95)",
+    },
+  };
+
   return (
-    <div>
-      <h2>This is banner</h2>
+    <div style={styles.banner}>
+      <div style={styles.content}>
+        <h2>This is banner</h2>
+      </div>
     </div>
   );
 };
